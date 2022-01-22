0760307e04645943d5f6aaa48e20ef97b0fb04c7
Michael Wang <michael.wang@hey.com>
2022-01-22 16:37:04 +0800

    change route according to auth state

--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -1,10 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repo_viewer/auth/application/auth_notifier.dart';
 import 'package:repo_viewer/auth/shared/providers.dart';
 import 'package:repo_viewer/core/presentation/routes/app_router.gr.dart';
 
 final initProvider = FutureProvider<Unit>((ref) async {
+  print('initProvider');
   final authNotifier = ref.read(authNotifierProvider.notifier);
   await authNotifier.updateAuthState();
   return unit;
@@ -20,10 +22,29 @@ class AppWidget extends StatelessWidget {
     return ProviderListener(
       provider: initProvider,
       onChange: (context, value) {},
-      child: MaterialApp.router(
-        title: 'Repo Viewer',
-        routerDelegate: appRouter.delegate(),
-        routeInformationParser: appRouter.defaultRouteParser(),
+      child: ProviderListener<AuthState>(
+        provider: authNotifierProvider,
+        onChange: (context, state) {
+          print('auth state: $state');
+          state.maybeMap(
+            authenticated: (_) {
+              appRouter.replaceAll([
+                const StarredReposRoute(),
+              ]);
+            },
+            unauthenticated: (_) {
+              appRouter.replaceAll([
+                const SignInRoute(),
+              ]);
+            },
+            orElse: () {},
+          );
+        },
+        child: MaterialApp.router(
+          title: 'Repo Viewer',
+          routerDelegate: appRouter.delegate(),
+          routeInformationParser: appRouter.defaultRouteParser(),
+        ),
       ),
     );
   }
