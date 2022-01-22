ee461cc2243de50e4b7f131ff4652128219f74ab
Michael Wang <michael.wang@hey.com>
2022-01-22 20:43:02 +0800

    build sign in page

--- lib/auth/presentation/sign_in_page.dart
+++ lib/auth/presentation/sign_in_page.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 48.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Icon(
+                    MdiIcons.github,
+                    size: 150.0,
+                  ),
+                  const SizedBox(height: 16.0),
+                  Text(
+                    'Welcome to\nRepo Viewer',
+                    style: Theme.of(context).textTheme.headline3,
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 32.0),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Sign in'),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
